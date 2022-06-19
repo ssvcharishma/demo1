c9e30c7492abdf8510ca673e4d7646c9f3aa66a6
charishma <sabbavarapucharishma@gmail.com>
2022-06-19 16:24:57 +0530

    initial commit

--- p1.js
+++ p1.js
@@ -1,1 +1,16 @@
-    alert("hello!");
+alert("hello javascript");
+document.write("hi\n\n");
+
+
+//operating addition
+var a = 10; //initialising a value
+var b = 30; //initialising a value
+var c = a + b; //adding the numbers
+document.write("\n\nc = " + c); //prints the value of  c on adding a and b 
+
+//another
+var i = 1;
+while (i <= 5) {
+    document.write(i + "\n\n");
+    i++;
+}
